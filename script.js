1b5c01820c65db9f5d62c3840cd50b17263b268b
yeicokmc <d.vinedocrew@gmail.com>
2024-08-20 19:21:02 -0400

    agregando edicion y eiliminacion de todos

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 const tasksContainers = document.querySelector("#task-container");
-const addTaskBtn = document.querySelector("#task-ask-btn");
+const form = document.querySelector("#task-input");
 const taskEditModal = document.querySelector(".task-edit-modal");
 const taskEditBtn = document.querySelector("#task-edit-btn");
 const taskEditBtnClosev = document.querySelector("#task-edit-btn-close");
+let todosLosTodo = [];
 
-//taskEditModal.showModal();
+// LOAD TASKS
+function loadTasks() {
+  todosLosTodo = JSON.parse(localStorage.getItem("tasks"));
 
-//AGREGAR TODO
-addTaskBtn.addEventListener("click", function (e) {
+  if (todosLosTodo && todosLosTodo.length > 0) {
+    const listaDeTodos = todosLosTodo.map((task) =>
+      createTaskItem(task.task_description, task.id)
+    );
+    tasksContainers.insertAdjacentHTML("beforeend", listaDeTodos.join(""));
+  }
+}
+loadTasks();
+
+// AGREGAR TO-DO
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
   const task = document.querySelector("#task").value;
-  console.log(task);
-  if (task === "") {
-    alert("por favor escribe un task");
+  if (!task) {
+    alert("Por favor escribe un task");
     return;
   }
+
   const id = createId();
   const html = createTaskItem(task, id);
 
   tasksContainers.insertAdjacentHTML("beforeend", html);
   document.querySelector("#task").value = "";
+
+  todosLosTodo.push({
+    id: id,
+    task_description: task,
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(todosLosTodo));
 });
 
 //EDITAR TO-DO CON (EVEN DELEGATION)
@@ -27,16 +48,29 @@ tasksContainers.addEventListener("click", function (e) {
   const target = e.target;
   const li = target.closest("li");
   if (target.dataset.edit) {
-    //  taskEditModal.showModal();
-    // taskEditModal.dataset.taskId = target.dataset.edit;
-    const newTask = prompt("escribe tu todo");
-    if (!newTask) return;
-
-    const p = li.querySelector(".task-description");
-    p.textContent = newTask;
+    taskEditModal.showModal();
+    taskEditModal.dataset.task_id = target.dataset.edit;
+    //const newTask = prompt("escribe tu todo");
+    // if (!newTask) return;
+    //  const p = li.querySelector(".task-description");
+    // p.textContent = newTask;
+  }
+  if (target.dataset.delete) {
+    tasksContainers.removeChild(li);
   }
 });
 
+taskEditBtn.addEventListener("click", function (e) {
+  const newDescription = document.querySelector("#task-edit").value;
+  if (!newDescription) return;
+
+  const taskId = taskEditModal.dataset.task_id;
+  todosLosTodo = todosLosTodo.map((task) => 
+  if (task.id == taskId))
+});
+taskEditBtnClosev.addEventListener("click", function (e) {
+  taskEditModal.close();
+});
 //helper functions
 function createTaskItem(task, id) {
   return `
